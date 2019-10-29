0098ba310c68e677c61aef09fcd464d7d670772b
donellabarcelo <donellabarcelo95@gmail.com>
2019-10-29 10:35:04 -0700

    Added my name, hobbies, fiances name and age, and our wedding date

--- AboutNella.playground/Contents.swift
+++ AboutNella.playground/Contents.swift
@@ -1,3 +1,12 @@
 import UIKit
 
 var str = "Hello, playground"
+
+let myName = "Nella"
+let hobbies = "iOS development, photography, OneWheeling"
+
+let myFiancesName = "Mary Ingram"
+var herAge = 23
+
+let dateOfMarriage = "November 1, 2020"
+
